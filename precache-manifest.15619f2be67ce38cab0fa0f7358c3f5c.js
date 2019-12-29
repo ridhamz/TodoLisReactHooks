b16cf15c10ab6dd8050d3c3367af87a421d15762
Ridha Mezrigui <47607249+ridhamz@users.noreply.github.com>
2019-12-29 21:32:05 +0100

    Updates

---
+++ precache-manifest.15619f2be67ce38cab0fa0f7358c3f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ef23bfe19b5e4e9b5ae96531f0fa73",
+    "url": "/TodoLisReactHooks/index.html"
+  },
+  {
+    "revision": "9a97b3241ed1dc73d917",
+    "url": "/TodoLisReactHooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b00e0904d2c7422f89d",
+    "url": "/TodoLisReactHooks/static/js/2.5ea5ef8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TodoLisReactHooks/static/js/2.5ea5ef8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a97b3241ed1dc73d917",
+    "url": "/TodoLisReactHooks/static/js/main.1ffa087b.chunk.js"
+  },
+  {
+    "revision": "719094f75395de393f59",
+    "url": "/TodoLisReactHooks/static/js/runtime-main.3f0e77e6.js"
+  }
+]);
